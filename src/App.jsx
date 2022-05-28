88e5d901d3954d3306642ce1d120dfb2611bd02c
BenoitSafari <gregory.michalak.192@gmail.com>
2022-05-28 12:40:01 +0200

    init github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { RouterScrollReset } from '@lib/RouterScrollReset';
-import Layout from '@components/Layout/Layout';
 import Home from '@pages/Home';
-import LibTest from '@pages/LibTest';
 import '@styles/reset.scss';
 import '@styles/globals.scss';
 
@@ -10,13 +7,9 @@ function App() {
   return(
     <>
       <Router>
-        <RouterScrollReset/>
-        <Layout>
-          <Routes>
-            <Route path='*' element={<LibTest />}/>
-            {/* <Route path='*' element={<Home />}/> */}
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route path='*' element={<Home />}/>
+        </Routes>
       </Router>
     </>
   );
--- src/lib/Input.jsx
+++
@@ -1,6 +0,0 @@
-import { Button } from './InputButton';
-import { SearchBar } from './InputSearchBar';
-export {
-  Button,
-  SearchBar,
-};
