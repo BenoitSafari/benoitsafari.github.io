349b70799a730499ccb3a2b7c48a50b5e83df7cb
BenoitSafari <gregory.michalak.192@gmail.com>
2022-06-15 17:53:46 +0200

    Updates

--- static/js/react-syntax-highlighter_languages_refractor_ini.677f83df.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbenoitsafari_github_io=self.webpackChunkbenoitsafari_github_io||[]).push([[6495],{3409:function(t){function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.677f83df.chunk.js.map
