e6f80098d9df6311321337db875b814d4a356048
benoitSafari <gregory.michalak.192@gmail.com>
2022-06-20 10:42:38 +0200

    <feature> useFetch OK

--- src/components/Lib_useFetch.tsx
+++ src/components/Lib_useFetch.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import useFetch from 'lib/useFetch';
-import { Container, H3, Usage, State } from './GlobalStyle';
+import { Container, H2, H3, Usage, State } from './GlobalStyle';
 import { InputText, Button } from 'lib/Inputs';
 import Loader from './Loader';
 import CodeBlock from './CodeBlock';
@@ -9,7 +9,7 @@ function LibInputs () {
   const [inputText, setInputText] = React.useState('Pikachu');
   const [query, setsetQuery]      = React.useState('pikachu');
   const {data, error, loading}    = useFetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-  const handleSearch = () => {setsetQuery(inputText);};
+  const handleSearch = () => {setsetQuery(inputText.toLowerCase());};
   return(
     <>
       <Container>
@@ -28,22 +28,92 @@ function LibInputs () {
         </div>
         <State>State: 
           {data?.count && <span>{data.count} results</span>}
-          {data?.name && <span>{data.name}</span>}
+          {data?.name && <span>Found n°{data.id} - {data.name}</span>}
           <div style={{marginTop: '.5rem'}}>error:&nbsp;{(error) ? <span style={{color: 'red'}}>{String(error)}</span> : <span>null</span> }</div>
           <div style={{marginTop: '.5rem'}}>loading:&nbsp;{(loading) ? <span style={{color: 'green'}}>true</span> : <span>false</span> }</div>
         </State>
         <details>
           <Usage>Usage</Usage>
           <CodeBlock>
-            {codeTest[0]}
+            {codeBlocks[0]}
+          </CodeBlock>
+        </details>
+        <H2>Download Library</H2>
+        <details>
+          <Usage>useFetch.tsx</Usage>
+          <CodeBlock>
+            {codeBlocks[1]}
           </CodeBlock>
         </details>
       </Container>
-
     </>
   );
 }
 
-const codeTest = [``];
+const codeBlocks = [`~~~jsx
+import React from 'react';
+import useFetch from '...path-to-useFetch';
+
+function MyComponent () {
+  const requestInit = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({someData: 'Lol'})
+  }
+  const {data, error, loading} = useFetch('https://url-to-fetch', requestInit);
+
+  return(
+    <>
+      {loading && <div>Please wait</div>}
+      {data && <div>{data.someStuff}</div>}
+    </>
+  )}`,
+`~~~jsx
+import { useState, useEffect } from 'react';
+
+/**
+ * @param {string}      url   url to fetch
+ * @param {RequestInit} init  request init object
+ */
+function useFetch(url: string, init?: RequestInit) {
+  const [data, setData]       = useState<any>(null);
+  const [error, setError]     = useState<unknown | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    (async () => {
+      // Reset states
+      setData(null);
+      setError(null);
+      setLoading(true);
+
+      try {
+        const res = await fetch(url, init);
+        if (res.ok) {
+          // Success
+          const data = await res.json();
+          setData(data);
+        } else {
+          // Error
+          throw Error(\`$\{res.status} $\{res.statusText}\`);
+        }
+
+      } catch(err) {
+        // Deal with error
+        setError(err);
+
+      } finally {
+        setLoading(false);
+      }
+
+    })();
+
+  }, [url, init]);
+  
+  return {data, error, loading};
+}
+
+export default useFetch;`
+];
 
 export default LibInputs;
--- src/lib/useFetch.ts
+++ src/lib/useFetch.ts
@@ -1,32 +1,44 @@
 import { useState, useEffect } from 'react';
 
-function useFetch (url: string) {
+/**
+ * @param {string}      url   url to fetch
+ * @param {RequestInit} init  request init object
+ */
+function useFetch(url: string, init?: RequestInit) {
   const [data, setData]       = useState<any>(null);
   const [error, setError]     = useState<unknown | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     (async () => {
+      // Reset states
       setData(null);
       setError(null);
       setLoading(true);
+
       try {
-        const res = await fetch(url);
+        const res = await fetch(url, init);
         if (res.ok) {
+          // Success
           const data = await res.json();
           setData(data);
         } else {
-          // Create error message
+          // Error
           throw Error(`${res.status} ${res.statusText}`);
         }
+
       } catch(err) {
-        // Deal with your error
+        // Deal with error
         setError(err);
+
       } finally {
         setLoading(false);
       }
+
     })();
-  }, [url]);
+
+  }, [url, init]);
+  
   return {data, error, loading};
 }
 
